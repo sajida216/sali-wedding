3c844ffd1c75de47e18e5112973a6948fd9c0f9d
sajida216 <ayishathsajidha@gmail.com>
2024-11-19 07:34:29 +0530

    Updates

---
+++ precache-manifest.c0e0fe16e6e7be9fb73cdd723b3e4603.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62fb9533ab244980d3fa4aa50df7dfe4",
+    "url": "%3Csajida216%3E.github.io/%3Csali-wedding%3E/index.html"
+  },
+  {
+    "revision": "d54efcd12c2d69abbe84",
+    "url": "%3Csajida216%3E.github.io/%3Csali-wedding%3E/static/css/2.83d756e6.chunk.css"
+  },
+  {
+    "revision": "c76d2e30f6323609cca0",
+    "url": "%3Csajida216%3E.github.io/%3Csali-wedding%3E/static/css/main.c4a148ef.chunk.css"
+  },
+  {
+    "revision": "d54efcd12c2d69abbe84",
+    "url": "%3Csajida216%3E.github.io/%3Csali-wedding%3E/static/js/2.973501cb.chunk.js"
+  },
+  {
+    "revision": "2133a33f4e9282dbf343",
+    "url": "%3Csajida216%3E.github.io/%3Csali-wedding%3E/static/js/3.c60c86f6.chunk.js"
+  },
+  {
+    "revision": "c76d2e30f6323609cca0",
+    "url": "%3Csajida216%3E.github.io/%3Csali-wedding%3E/static/js/main.c933bfe0.chunk.js"
+  },
+  {
+    "revision": "6a3917d837e01e89b798",
+    "url": "%3Csajida216%3E.github.io/%3Csali-wedding%3E/static/js/runtime~main.a42ece4a.js"
+  },
+  {
+    "revision": "40063720e7ca51d2b222e4455ab4afc8",
+    "url": "%3Csajida216%3E.github.io/%3Csali-wedding%3E/static/media/bg21.40063720.jpg"
+  },
+  {
+    "revision": "cb54c796d7a425a28a67d90e54569aae",
+    "url": "%3Csajida216%3E.github.io/%3Csali-wedding%3E/static/media/img1.cb54c796.JPG"
+  },
+  {
+    "revision": "74e3f6ac4db2d67a5ad25eaed1d009ea",
+    "url": "%3Csajida216%3E.github.io/%3Csali-wedding%3E/static/media/img2.74e3f6ac.JPG"
+  }
+]);
